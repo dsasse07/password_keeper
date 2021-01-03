4a6f9fdb4644faa3cc12b3ffaf6c485ee02e4672
Daniel Sasse <dsasse07@gmail.com>
2021-01-03 13:00:16 -0500

    Send SMS via Twilio setup

---
+++ app/tools/twilio.rb
@@ -0,0 +1,25 @@
+# Download the twilio-ruby library from twilio.com/docs/libraries/ruby
+require 'pry'
+require 'dotenv/load'
+require 'twilio-ruby'
+
+module TwilioControls
+
+  # To set up environmental variables, see http://twil.io/secure
+  @@account_sid = ENV['TWILIO_ACCOUNT_SID']
+  @@auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new(@@account_sid, @@auth_token)
+
+  from = '+12676338725' # Your Twilio number
+  to = '+16464790409' # Your mobile phone number
+
+  def send_sms(to:, body:)
+    @client.messages.create(
+    from: from,
+    to: to,
+    body: "Hello\n\nWorld"
+    )
+  end
+
+end
